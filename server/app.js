7c37aa788fccccfd37c96d9a91962d9018740d56
Anthonythuo <antonykariuki@yahoo.com>
2017-10-26 18:33:04 +0100

    solved the merge conflict on serversetupadmindummypage

--- server/app.js
+++ server/app.js
@@ -27,11 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
- serverSetUpAdminDummyPage
 app.use('/admin', admin);
 
 app.use('/api', router);
- master
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
