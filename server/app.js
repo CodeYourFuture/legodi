655c49adaacbec82e020f2b7f41f72656b650803
Anthony Thuo <antonykariuki@yahoo.com>
2017-10-26 18:45:06 +0100

    setup dummy admin page (#14)
    * serverSetUpAdminDummyPage
    
    * solved the merge conflict on serversetupadmindummypage

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var router = require('./routes/api');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -26,17 +27,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/admin', admin);
+
 app.use('/api', router);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
