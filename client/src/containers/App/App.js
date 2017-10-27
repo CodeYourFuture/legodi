f86c4473cd2dcea91c0d52982fbff7080478b402
kharazzoro <kha.1991.14.2017@gmail.com>
2017-10-27 13:09:57 +0100

    setup react route (#11)
    * setup react route
    
    *  change component folder to components

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import { Router, Route, Switch } from 'react-router'
+import { BrowserRouter as Router , Route, Link } from "react-router-dom";
+ 
+import Home from '../../components/home.js'
+import About from '../../components/about.js'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+         </ul>
+  
+        <hr/>
+  
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+       </div>
+    </Router>
+      
     );
   }
 }
