24d1765cfb6db2dc7084c0583113fdcb114c6a8b
MrDwina <diwina1993@hotmail.com>
2017-10-28 15:14:43 +0100

    added two endpoint for addArticle

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const addCategory = require('../dbClients/categoriesDB');
+const addarticle = require('../dbClients/articlesDB')
 
 router.get("/", function (req, res, next) {
   res.send('admin');
 });
 
+//Add category
 router.get("/categories/add", function (req, res, next) {
   res.render('category');
 });
@@ -18,6 +20,20 @@ router.post('/categories/add', function (req, res, next) {
   }
   addCategory(query, callback);
 });
+
+// Add article
+router.get('/article/add', (req, res, next) => {
+     res.render('add-articles');
+  });
+
+router.post('/article/add', (req, res) => {
+  const query = req.body;
+    const callBack = (data) => {
+        res.redirect('/')
+      res.end();
+    }
+    addarticle(query, callBack)
+})
 
 module.exports = router;
 
