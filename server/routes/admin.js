76ab2a637e6af1d9af074527ea2f84d2b9546ec8
MrDwina <diwina1993@hotmail.com>
2017-10-28 10:34:10 +0100

    added two endpoint git and post

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const saveCategory = require('../dbClients/categoriesDB');
 
-router.get('/', function (req, res, next) {
-  res.send('admin');
+router.get("/", function (req, res, next) {
+  res.render('addCategory');
+});
+
+router.post('/', function (req, res, next) {
+  const query = req.body;
+  const callback = () => {
+    res.redirect("/");
+    res.end()
+  }
+  saveCategory(query, callback);
 });
 
 module.exports = router;
