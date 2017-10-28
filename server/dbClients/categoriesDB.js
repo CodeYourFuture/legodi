df890c3a7d86188ec5000447832ff1f891480b94
MrDwina <diwina1993@hotmail.com>
2017-10-28 13:58:54 +0100

    chenged model name

--- server/dbClients/categoriesDB.js
+++ server/dbClients/categoriesDB.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const AddCategory = require('../models/Category');
+const CategoryModel = require('../models/Category');
 
 const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/legodi';
 mongoose.Promise = global.Promise;
 
-const saveCategory = (query, callback) => {
+const category = (query, callback) => {
     mongoose.connect(mongoConnection);
-    AddCategory.create(query).then(callback)
+    CategoryModel.create(query).then(callback)
 }
 
-module.exports = saveCategory;
+module.exports = category;
 
 
