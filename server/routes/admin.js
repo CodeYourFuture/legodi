6b68efda822fbd34103707f544ce4b20825ad89e
MrDwina <diwina1993@hotmail.com>
2017-10-28 19:16:06 +0100

    fixed conflict

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const addCategory = require('../dbClients/categoriesDB');
+const categoryClint = require('../dbClients/categoriesDB');
 const articleClient = require('../dbClients/articlesDB')
 
 
@@ -18,7 +18,7 @@ router.post('/categories/add', function (req, res, next) {
     res.redirect("/");
     res.end()
   }
-  addCategory(query, callback);
+  categoryClint.categoryClint(query, callback);
 });
 
 router.get('/article/add', (req, res, next) => {
