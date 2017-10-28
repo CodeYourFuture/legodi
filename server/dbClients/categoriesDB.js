378509e5b09ec89ee88f1d8ec42a67b4cb256558
MrDwina <diwina1993@hotmail.com>
2017-10-28 10:35:13 +0100

    created function to save data into db

---
+++ server/dbClients/categoriesDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const AddCategory = require('../models/Category');
+
+const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/legodi';
+
+const saveCategory = (query, callback) => {
+    mongoose.connect(mongoConnection);
+    AddCategory.create(query).then(callback)
+}
+
+module.exports = saveCategory;
+
+
