b980ff20402a32089a6c93751b88b64a74612fb6
kharazzoro <kha.1991.14.2017@gmail.com>
2017-10-29 21:49:10 +0000

    update process.env to be REACT_APP_API_URI

--- client/src/apiAxios/apiClient.js
+++ client/src/apiAxios/apiClient.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const REACT_APP_API_URI  = process.env.API_URI || 'http://localhost:3001';
+const apiUrl = process.env.REACT_APP_API_URI || 'http://localhost:3001';
 
 const apiClient = {
     getCategories: () => {
-        return axios.get(`${REACT_APP_API_URI }/api/categories`);
+        return axios.get(`${apiUrl}/api/categories`);
     },
     getArticles: () => {
-        return axios.get(`${REACT_APP_API_URI }/api/articles`);
+        return axios.get(`${apiUrl}/api/articles`);
     }
 }
 
