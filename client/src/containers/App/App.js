a446583eaa09a37a6678c0258f0994a995de2f5e
kharazzoro <kha.1991.14.2017@gmail.com>
2017-10-30 18:42:52 +0000

    add header and category nav reactstrap (#33)
    * add header and category nav reactstrap
    
    * create header.css
    
    * improve naves and create categories foler
    
    * put each component inside folder
    
    * change axios folder to be Helper and edit the css
    
    *  change Helper folder name to Helpers
    
    * organize folder for each component hold or files related to this component
    
    * delete headeCss folder change Helper to helpr

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Menu from '../../components/menu.js'
-//import Home from '../../components/home.js'
-import About from '../../components/about.js'
-import Categories from '../../components/categories.js'
+import Menu from '../../components/Menu/menu.js'
+import About from '../../components/About/about.js'
+import Header from '../../components/Header/header.js'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CategoriesOverview from "../CategoriesOverview/CategoriesOverview"
- 
+import CategoriesOverview from "../../components/Categories/CategoriesOverview/CategoriesOverview"
+import CategoriesNavOverview from "../../components/Categories/CategoriesOverview/categoriesNavOverView"
+
 class App extends Component {
   render() {
     return (
 
       <Router>
         <div>
+           <Header headerTitle={"Legodi"}
+            headerDesc={"Digital Welcome Pack For Refugee in Glasgow"} />
           <Menu   />
           <hr />
-          <Route exact path="/" component={CategoriesOverview} />
+          <Route exact path="/" component={CategoriesNavOverview} />
           <Route path="/about" component={About} />
-          <Route path="/categories" component={Categories} />
+          <Route path="/categories" component={CategoriesOverview} />
+ 
         </div>
       </Router>
 
