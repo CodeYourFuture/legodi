374a53e1a7d724a7c467a6a2ff4a298322e43125
MrDwina <diwina1993@hotmail.com>
2017-11-01 22:05:59 +0000

    added category schema to article schema

--- server/models/Article.js
+++ server/models/Article.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
+const CategoryModel = require('../models/Category');
 const { Schema } = mongoose;
 
 const schema = new Schema({
     title: String,
-    fullContent: String
+    fullContent: String,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: CategoryModel
+    },
+    categoryName: [{
+        text: String,
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: CategoryModel
+        }
+    }]
 });
 
 const Article = mongoose.model('articles', schema);
