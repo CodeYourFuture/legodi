bc8204b6f6ec484fde987efb80a5d20263da08e0
MrDwina <diwina1993@hotmail.com>
2017-11-02 20:48:05 +0000

    Refactor admin (#46)
    * deleted admin and user route
    
    * created new folder and file for articles
    
    * created new folder and file for categories
    
    * deleted admin and user routes

---
+++ server/routes/admin/categories/categories.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const categoryClient = require('../../../dbClients/categoriesDB');
+const articleClient = require('../../../dbClients/articlesDB')
+const ObjectId = require('mongodb').ObjectID;
+
+
+router.get('/', function (req, res, next) {
+    const callback = (error, category) => {
+        res.render("admin-list-categories", {
+            category
+        })
+    }
+    categoryClient.findCategories({}, callback);
+});
+
+router.get("/add", function (req, res, next) {
+    res.render('admin-add-category');
+});
+
+router.post('/add', function (req, res, next) {
+    const query = req.body;
+    const callback = () => {
+        res.redirect("/");
+    }
+    categoryClient.addCategory(query, callback);
+});
+
+router.get('/edit/:categoryId', (req, res) => {
+    let categoryId = req.params.categoryId;
+    const callback = (error, category) => {
+        res.render("admin-edit-category", {
+            category: category
+        })
+    }
+    categoryClient.findCategoryById(categoryId, callback);
+});
+
+router.post('/edit/:categoryId', function (req, res, next) {
+    let categoryId = req.params.categoryId;
+    const query = req.body;
+
+    const callback = (error, category) => {
+        res.redirect('/')
+    }
+
+    categoryClient.editCategory(categoryId, query, true, callback);
+});
+
+module.exports = router;
