c3af6c0caaba4df6fa53e464fa20540149bd72a1
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2017-11-02 20:47:37 +0000

    add file article-page  in client

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -6,11 +6,11 @@ import Header from '../../components/Header/header.js'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import CategoriesOverview from "../../components/Categories/CategoriesOverview/CategoriesOverview"
 import CategoriesNavOverview from "../../components/Categories/CategoriesOverview/categoriesNavOverView"
+import SingleArticle from "../../containers/SingleArticle/SingleArticle";
 
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div>
            <Header headerTitle={"Legodi"}
@@ -19,12 +19,10 @@ class App extends Component {
           <hr />
           <Route exact path="/" component={CategoriesNavOverview} />
           <Route path="/about" component={About} />
-          <Route path="/categories" component={CategoriesOverview} />
- 
+          <Route path="/categories" component={CategoriesOverview} /> 
+          <Route path="/articles/:articleId" component={SingleArticle} />
         </div>
       </Router>
-
-
     );
   }
 }
