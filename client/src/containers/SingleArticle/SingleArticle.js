4a9d3e1560801f283f69fd64c462cc7e99e088fb
MrDwina <diwina1993@hotmail.com>
2017-11-06 18:42:45 +0000

    added detals to singleArticle page

--- client/src/containers/SingleArticle/SingleArticle.js
+++ client/src/containers/SingleArticle/SingleArticle.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import apiClient from '../../helpers/apiClient'
 
 class SingleArticle extends Component {
+  
+  constructor() {
+
+    super();
+    this.state = {
+      articles: []
+    }
+  }
+  componentDidMount() {
+    apiClient.getArticles()
+      .then(({ data }) => {
+        this.setState({
+          articles: data
+        })
+      })
+      .catch((err) => { })
+  }
+
   render() {
     const { articleId } = this.props.match.params;
     return (
-      <div className="SingleArticle">
-          SingleArticle {articleId}
+      <div>
+        {this.state.articles.map(article => {
+          if (article._id === articleId) {
+            return <div>
+              <h5>{article.title}</h5>
+              <p>{article.fullContent}</p>
+            </div>
+          }
+        })}
       </div>
     )
   }
