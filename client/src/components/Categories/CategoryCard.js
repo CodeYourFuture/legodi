5155649667e2daaf7346e8f7d2ce713faa8359d2
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2017-11-18 13:31:48 +0000

    Implement Design for Category

--- client/src/components/Categories/CategoryCard.js
+++ client/src/components/Categories/CategoryCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Card, CardText, CardBody,CardTitle, Button } from 'reactstrap';
+import { Card, CardText, CardBody,CardTitle, Button, CardImg, style } from 'reactstrap';
+
+
 function CategoryCard(props){ 
  return (
         <div>
           <Card>
             <CardBody>
-              <CardTitle>Title: {props.category.title}</CardTitle>
+              <CardImg img={{uri: 'asset:/app_icon.png'}} style={{width: 40, height: 40}}></CardImg>
+              <CardTitle style={{color:'#0f352e'}} >Title: {props.category.title} </CardTitle>
               <CardText>Description:{props.category.shortDescription}</CardText>
               <Button onClick={() => props.history.push(props.href)}>view category</Button>
             </CardBody>
