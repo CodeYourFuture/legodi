0ea0d3b45fef9862ed2c168b28f5752e959d2f92
MrDwina <diwina1993@hotmail.com>
2017-11-19 11:07:48 +0000

    fixed conflict

--- client/src/components/Categories/CategoryIcon.js
+++ client/src/components/Categories/CategoryIcon.js
@@ -5,11 +5,8 @@ import { Link } from 'react-router-dom';
 function CategoryIcon(props) {
     return (
         <div className="category-icon">
-<<<<<<< HEAD
             <div className="icon-border"></div>
             <div className="icon-border-cros"></div>
-=======
->>>>>>> 45a2f13a3da2388636b49b36c31b5d5f3c8a96d3
             <Link to={props.href}><img src={props.src} alt={props.alt} /></Link>
         </div>
     );
