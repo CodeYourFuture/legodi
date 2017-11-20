0ce7b5e50a75c93b83be02d8f9ad47692e72ee24
cardona6 <hervin_rambito@hotmail.com>
2017-11-20 00:14:36 +0000

    Implement Design for Category (#60)
    * Implement Design for Category
    
    * Design For Categorhy View

--- client/src/components/Categories/CategoryCard.js
+++ client/src/components/Categories/CategoryCard.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import { CardLink, Row, Card, CardText, CardBody, CardTitle, Button, CardImg, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './CategoryCard.css';
+
+function CategoryCard(props) {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <Row>
+            <Col sm="2">
+              <CardImg img={{ uri: 'asset:/app_icon.png' }} style={{ width: 200, height: 200 }}></CardImg>
+            </Col>
+            <Col sm="2">
+              <Link className="category-link" to={props.href}>
+                <CardLink>
+                  <CardTitle style={{ color: '#0f352e' }} > {props.category.title} </CardTitle>
+                  <CardText>{props.category.shortDescription}</CardText>
+                </CardLink>
+              </Link>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
 import './CategoriesOverview/category.css';
 import CategoryIcon from './CategoryIcon'
 
