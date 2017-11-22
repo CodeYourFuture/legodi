2280423769d1044e381eb49e8d0e5ded675a8f6d
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2017-11-22 17:02:54 +0000

    change the icon

--- client/src/components/Categories/CategoryCard.js
+++ client/src/components/Categories/CategoryCard.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { CardLink, Row, Card, CardText, CardBody, CardTitle, Button, CardImg, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import CategoryIcon from './CategoryIcon';
-import './CategoriesOverview/category.css';
 import './CategoryCard.css';
 
 function CategoryCard(props) {
@@ -26,15 +24,6 @@ function CategoryCard(props) {
         </CardBody>
       </Card>
     </div>
-  )
+  );
 }
-
-
-  function CategoryCard(props) {
-    return (
-      <div className="category-card">
-        <CategoryIcon {...props} />
-      </div>
-    );
-  }
 export default CategoryCard;
