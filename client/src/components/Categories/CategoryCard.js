b46d299578fb64825c7b58b63edc066ac3819f13
Mozafar <kabaros@gmail.com>
2017-11-24 19:26:05 +0000

    Revert "Reverting category card (#71)"
    This reverts commit 752df25e923aa89163468594614f30fd630ecffc.

--- client/src/components/Categories/CategoryCard.js
+++ client/src/components/Categories/CategoryCard.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import { CardLink, Row, Card, CardText, CardBody, CardTitle, Button, CardImg, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CategoryIcon from './CategoryIcon';
 import './CategoryCard.css';
 
-function CategoryCard(props) {
-  return (
-    <div>
-      <Card>
-        <CardBody>
-          <Row>
-            <Col sm="2">
-              <CardImg img={{ uri: 'asset:/app_icon.png' }} style={{ width: 200, height: 200 }}></CardImg>
-            </Col>
-            <Col sm="2">
-              <Link className="category-link" to={props.href}>
-                <CardLink>
-                  <CardTitle style={{ color: '#0f352e' }} > {props.category.title} </CardTitle>
-                  <CardText>{props.category.shortDescription}</CardText>
-                </CardLink>
-              </Link>
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
-    </div>
-  )
+ function CategoryCard(props) {
+     return (
+       <div className="category-card">
+         <CategoryIcon {...props} />
+       </div>
+  );
 }
 export default CategoryCard;
