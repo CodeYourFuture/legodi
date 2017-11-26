6c77d55e5f689f5a031413acd55f182a6ea7cc84
MrDwina <diwina1993@hotmail.com>
2017-11-26 05:54:18 +0000

    updated categoryDropMenu to show just last five categories and order them the last category added to be on top of the dropMenu

--- client/src/components/Menu/CategoryDropMenu.js
+++ client/src/components/Menu/CategoryDropMenu.js
@@ -18,17 +18,25 @@ class CategoryDropMenu extends Component {
                 })
             })
     }
+    getCategoryDropList(pram) {
+        const lastFiveCategory = [];
+        if (pram.length > 0) {
+            for (let i = pram.length - 5; i < pram.length; i++) {
+                lastFiveCategory.push(pram[i]);
+            }
+        }
+        return lastFiveCategory.reverse();
+    }
+
     render() {
         return (
             <div className="category-menu-container">
                 <div className="overlay"></div>
                 {
-                    this.state.categoriesList.map((category) => {
-                        return (
-                            <div className="category-menu-item" key={category._id}>
-                                <CategoryDropMenuIcon alt={category.icon} src={`/icons/${category.icon}.png`} href={`/categories/${category._id}`} />
-                            </div>
-                        )
+                    this.getCategoryDropList(this.state.categoriesList).map((category) => {
+                        return <div className="category-menu-item" key={category._id}>
+                            <CategoryDropMenuIcon alt={category.icon} src={`/icons/${category.icon}.png`} href={`/categories/${category._id}`} />
+                        </div>
                     })
                 }
             </div>
