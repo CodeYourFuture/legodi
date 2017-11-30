112349cb4f81a56e00da9e95d348314ca915a176
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-30 13:31:36 +0000

    remove script and do the same functionalitey of the task using pure css

--- server/public/script/script.js
+++ server/public/script/script.js
@@ -1,8 +1,1 @@
-let articleListContent = document.getElementsByClassName('articleList-content');
-
-    for (var i = 0; i < articleListContent.length; i++) {
-
-         let content = articleListContent[i].innerHTML;
-        articleListContent[i].innerHTML = content.slice(0, 70) + " ...";
-   
-    }
+ 
