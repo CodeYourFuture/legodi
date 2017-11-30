b105297c310b00b8f3d8c7593886544e8f586b52
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-30 12:58:25 +0000

    remove condition

--- server/public/script/script.js
+++ server/public/script/script.js
@@ -2,8 +2,7 @@ let articleListContent = document.getElementsByClassName('articleList-content');
 
     for (var i = 0; i < articleListContent.length; i++) {
 
-      if (articleListContent[i].innerHTML.length > 70) {
-        let content = articleListContent[i].innerHTML;
+         let content = articleListContent[i].innerHTML;
         articleListContent[i].innerHTML = content.slice(0, 70) + " ...";
-      }
+   
     }
