223c50b71d05fc2087c8d45cbf10069f1a23af86
MrDwina <diwina1993@hotmail.com>
2017-12-06 15:31:25 +0000

    fixed category dromenu issues

--- client/src/components/Menu/CategoryDropMenuIcon.js
+++ client/src/components/Menu/CategoryDropMenuIcon.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import './menu.css';
+import { withRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function CategoryDropMenuIcon(props) {
     return (
         <div className="category-card">
             <div className="category-icon">
-                <Link to={props.href}><img src={props.src} alt={props.alt} /></Link>
+                <button conClick={() => {props.history.push('/categories/5a237266457b4c069453ec68') }} to={props.href}><img src={props.src} alt={props.alt} /></button>
             </div>
         </div>
     );
