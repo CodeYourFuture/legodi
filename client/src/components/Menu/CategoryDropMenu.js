21666d28e2fdab50885466ee4e5c16d26fb61df1
MrDwina <diwina1993@hotmail.com>
2017-12-06 22:51:55 +0000

    used a HTML element tag instade of react Link

--- client/src/components/Menu/CategoryDropMenu.js
+++ client/src/components/Menu/CategoryDropMenu.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Link } from 'react';
 import CategoryDropMenuIcon from "./CategoryDropMenuIcon";
 import apiClient from '../../../src/helpers/apiClient';
 import './menu.css';
+
 
 class CategoryDropMenu extends Component {
     constructor() {
@@ -26,7 +27,11 @@ class CategoryDropMenu extends Component {
                 {
                     this.state.categoriesList.slice(this.state.categoriesList.length - 5).reverse().map((category) => {
                         return <div className="category-menu-item" key={category._id}>
-                            <CategoryDropMenuIcon alt={category.icon} src={`/icons/${category.icon}.png`} href={`/categories/${category._id}`} />
+                                <div className="category-card">
+                                    <div className="category-icon">
+                                        <a href={`/categories/${category._id}`}><img src={`/icons/${category.icon}.png`} alt={category.icon} /></a>
+                                    </div>
+                                </div>
                         </div>
                     })
                 }
--- client/src/components/Menu/CategoryDropMenuIcon.js
+++ client/src/components/Menu/CategoryDropMenuIcon.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './menu.css';
-import { withRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+// import React, { Component } from 'react';
+// import './menu.css';
+// import { withRouter } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 
-function CategoryDropMenuIcon(props) {
-    return (
-        <div className="category-card">
-            <div className="category-icon">
-                <button conClick={() => {props.history.push('/categories/5a237266457b4c069453ec68') }} to={props.href}><img src={props.src} alt={props.alt} /></button>
-            </div>
-        </div>
-    );
-}
+// function CategoryDropMenuIcon(props) {
+//     <Route render={({ history }) => (
+//         <div className="category-card">
+//             <div className="category-icon">
+//                 <button conClick={() => { props.history.push('/categories/5a237266457b4c069453ec68') }} to={props.href}><img src={props.src} alt={props.alt} /></button>
+//             </div>
+//         </div>
+//     )} />
+// }
 
-export default CategoryDropMenuIcon;
+// export default CategoryDropMenuIcon;
