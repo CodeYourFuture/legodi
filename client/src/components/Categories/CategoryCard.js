60e587db89979a97fd8b44d1755463e0162b6bc1
DwinaTech <diwina1993@hotmail.com>
2017-12-10 20:36:26 +0000

    Drop menu title (#124)
    * edit category-card to be clicked in hall category-card warp
    
    * change category design
    
    * added title to category dropMenu

--- client/src/components/Categories/CategoryCard.js
+++ client/src/components/Categories/CategoryCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import CategoryIcon from './CategoryIcon';
 import './CategoryCard.css';
+import './CategoriesOverview/category.css';
+import { Link } from 'react-router-dom';
+
 
  function CategoryCard(props) {
-     return (
+   return (
+     <div className="main-category-item">
+     <Link to={props.href}>
        <div className="category-card">
-         <CategoryIcon {...props} />
-         {props.title}
+         <div className="category-icon">
+           <div className="icon-border"></div>
+           <div className="icon-border-cros"></div>
+           <img src={props.src} alt={props.alt} />
+         </div>
+         <h1 className="icon-title">{props.title}</h1>
        </div>
+       </Link>
+     </div>  
   );
 }
 export default CategoryCard;
+
