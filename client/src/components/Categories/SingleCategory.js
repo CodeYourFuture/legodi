ecd2c48f774719503d94d90db339a649544ef817
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-10 20:39:00 +0000

    single article full content (#125)
    * single article full content
    
    * article link/ articles to be article

--- client/src/components/Categories/SingleCategory.js
+++ client/src/components/Categories/SingleCategory.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import apiClient from '../../helpers/apiClient';
 import './CategoriesOverview/category.css'
+import { Link } from 'react-router-dom';
+
 
 class SingleCategory extends Component {
 
@@ -25,31 +27,46 @@ class SingleCategory extends Component {
                     })
                 } else {
                     const { category = {} } = data[0];
-                    this.setState({
+                     this.setState({
                         articles: data,
-                        category
+                         category
                     })
                 }
             })
     }
 
     render() {
+       
+        
         return (
+            
             <div className="single-category">
-                <h1 className="category-header">{this.state.category.title}</h1>    
+                <h1 className="category-header">{this.state.category.title}</h1> 
+               
+   
                 <p className="category-description">{this.state.category.description}</p>
                 {this.state.articles.map(article => {
-                    return <div key={article._id} className="article-content">    
+                     const articleLink="/articles/"+article._id;
+                    return <Link to={articleLink}> 
+                    <div key={article._id} className="article-content">  
+                       
                         <div className="article-logo">    
-                            <img src="/icons/default-icon.png" alt="default-icon" />
+                       
+                        <img src="/icons/default-icon.png" alt="default-icon" />
+                      
                         </div>
+                         
                         <div className="article-pragraph">
-                            <p>{article.fullContent}</p>
+                             <p>{article.fullContent}</p>
+                     
                         </div>    
                     </div>
+                    </Link>
                 })}
                 <h1>{this.state.defaultData}</h1>
+                
             </div>
+           
         )
     }
 }
