3221eae1697c32ec0fa26de1e0f035ffa84fe6da
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-12 11:13:53 +0000

    add root and questionsGame component

---
+++ client/src/containers/QuestionGame/question.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+ 
+class QuestionGames extends Component {
+ 
+    render() {
+
+        return (
+            <div > 
+                    Weegi Game
+            </div>
+        )
+    }
+}
+
+export default QuestionGames;
