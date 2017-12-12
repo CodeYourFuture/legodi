8a5c3178d9b7a1cf97616a541c82c19764c58d0f
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-12 11:26:32 +0000

    add single question component

---
+++ client/src/components/Question/question.js
@@ -0,0 +1,13 @@
+import React from 'react';   
+
+function Question() {
+ 
+        return (<div>
+                    <h1>Question</h1>
+                  </div>
+            
+        )
+ 
+}
+
+export default Question
--- client/src/containers/QuestionGame/question.js
+++ client/src/containers/QuestionGame/question.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
- 
+ import Question from '../../components/Question/question'
 class QuestionGames extends Component {
  
     render() {
 
         return (
-            <div > 
-                    Weegi Game
-            </div>
+                    <div >
+                        <Question  />
+                    </div>
         )
     }
 }
