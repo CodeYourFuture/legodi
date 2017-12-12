a5917bbf39a6a155a217c9b8fc52bcb5d202af56
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-12 14:07:34 +0000

    qestion game read json file

--- client/src/components/Question/question.js
+++ client/src/components/Question/question.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class Question extends React.Component {
   render() {
     return (
       <Form className="container">
         <FormGroup tag="fieldset" row>
-          <legend className="col-form-legend col-sm-2">Questin number:</legend>
+          <legend className="col-form-legend col-sm-2">{this.props.title}</legend>
           <Col sm={10}>
             <FormGroup check>
               <Label check>
                 <Input type="radio" name="radio1" />{' '}
-                Option one is this and that—be sure to include why it's great
+                {this.props.choices.a}
               </Label>
             </FormGroup>
             <FormGroup check>
               <Label check>
                 <Input type="radio" name="radio2" />{' '}
-                Option two can be something else and selecting it will deselect option one
-              </Label>
+                {this.props.choices.b}
+                </Label>
             </FormGroup>
             <FormGroup check>
               <Label check>
                 <Input type="radio" name="radio3" />{' '}
-                Option two can be something else and selecting it will deselect option one
-              </Label>
+                {this.props.choices.c}
+                </Label>
             </FormGroup>
             <FormGroup check>
               <Label check>
                 <Input type="radio" name="radio4" />{' '}
-                Option two can be something else and selecting it will deselect option one
-              </Label>
+                {this.props.choices.d}
+                </Label>
             </FormGroup>
           </Col>
           <Button color="primary">Submit</Button>
--- client/src/containers/QuestionGame/questiongmae.js
+++ client/src/containers/QuestionGame/questiongmae.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
  import Question from '../../components/Question/question'
+ import QuestionsObject from '../../data/questions.json'
+ var mydata = require("../../data/questions.json");
 class QuestionGames extends Component {
- 
+    constructor(){
+        super();
+        this.state = {
+            questions: []
+        }
+    }
+    componentDidMount(){
+         
+     let data=   mydata;
+     
+       this.setState({
+        questions:data
+      })
+     }
     render() {
-
+             
         return (
+           
                     <div >
-                        <Question  />
+                        {this.state.questions.map((question,i)=>{
+                            console.log(question.choices[0].a);
+                  return   <Question key={i} title={question.title} choices={question.choices[0]} answer={question.answer}/>
+                        })}
+                       
+                       
                     </div>
         )
     }
