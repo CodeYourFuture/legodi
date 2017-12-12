0fac3365672a54e9a09231b13d9ea9f11d62c35e
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-12 11:53:28 +0000

    add question samble for questions json

--- client/src/components/Question/question.js
+++ client/src/components/Question/question.js
@@ -1,13 +1,41 @@
-import React from 'react';   
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-function Question() {
- 
-        return (<div>
-                    <h1>Question</h1>
-                  </div>
-            
-        )
- 
+export default class Question extends React.Component {
+  render() {
+    return (
+      <Form className="container">
+        <FormGroup tag="fieldset" row>
+          <legend className="col-form-legend col-sm-2">Questin number:</legend>
+          <Col sm={10}>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" />{' '}
+                Option one is this and that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio2" />{' '}
+                Option two can be something else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio3" />{' '}
+                Option two can be something else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio4" />{' '}
+                Option two can be something else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+          </Col>
+          <Button color="primary">Submit</Button>
+        </FormGroup>
+      </Form>
+    );
+  }
 }
-
-export default Question
