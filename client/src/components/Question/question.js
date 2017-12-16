6da429f1876faf971e0e293af7415ac628e0b87d
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-16 21:00:15 +0000

    add weegie game to menu

--- client/src/components/Question/question.js
+++ client/src/components/Question/question.js
@@ -15,14 +15,16 @@ export default class Question extends React.Component {
       rightanswer: 0,
       wronganswer: 0,
       userAnswers:[],
-      gameQuestion:[]
+      gameQuestion:[],
+      correct:0,
+      wrong:0,
+      wrongAnswersList:[]
     }
   }
 
   componentDidMount(){
     apiClient.getWeegie()
     .then(({ data }) => {
-       console.log(data)
        this.setState({
         gameQuestion:data
        })
@@ -34,18 +36,27 @@ export default class Question extends React.Component {
     const questionNumber=this.state.questionNumber
 
     if(questionNumber === 9){
-       apiClient.postWeegie(this.state.userAnswers);
+       apiClient.postWeegie(this.state.userAnswers)
+       .then((response)=>{
+         console.log(response.data)
+         this.setState({
+          rightanswer:response.data.correctAnswers,
+          wronganswer:response.data.wrongAnswers,
+          wrongAnswersList:response.data.wrongAnswersList
+          
+         });
+       })
         
       
     } 
       
     if (answerValue === this.state.selectedOption) {
       this.setState({
-        rightanswer: this.state.rightanswer + 1
+        correct: this.state.correct + 1
       })
     } else {
       this.setState({
-        wronganswer: this.state.wronganswer + 1
+        wrong: this.state.wrong + 1
       })
     }
     this.setState({
@@ -78,11 +89,10 @@ export default class Question extends React.Component {
     }
 
     if (this.state.questionNumber < 10) {
-     
-      return (
+       return (
 
         <Form className="container" >
-          <h3 class="text-success"> Correct:<span class="text-danger">{this.state.rightanswer}</span>  <span class="text-info">|</span> Wrong:<span class="text-danger">{this.state.wronganswer}</span></h3>
+          <h3 class="text-success"> Correct:<span class="text-danger">{this.state.correct}</span>  <span class="text-info">|</span> Wrong:<span class="text-danger">{this.state.wrong}</span></h3>
           <h2 className=" text-primary">Question <span class="text-danger">{this.state.questionNumber + 1}</span></h2>
           <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-4 text-primary"><h3>Calculate :<span class="text-danger">{questionTitle}</span></h3></legend>
@@ -119,11 +129,31 @@ export default class Question extends React.Component {
 
         </Form>
       );
-    } else {
+    } else {      
        return (
         <div class="container">
           <h3 class="text-center text-success">True answers:{this.state.rightanswer}</h3>
           <h3 class="text-center text-success">Wrong answers:{this.state.wronganswer}</h3>
+          { this.state.wrongAnswersList.map((question,i)=>{
+            return (
+              <ul class="pb-5" key={question._id}>
+               <h2>correction {i+1}:</h2> 
+            <li >Question: {question.title}</li>
+            
+            <li>a:{question.choices.a}</li>
+           
+            <li>b:{question.choices.b}</li>
+            
+            <li>c:{question.choices.c}</li>
+            
+            <li>d;{question.choices.d}</li>
+
+            <h3 class="text-danger">Answer: {question.answer} </h3>
+
+
+            </ul>
+          )
+          })}
         </div>
       );
     }
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const categoryClient = require("../dbClients/categoriesDB");
 const articleClient = require('../dbClients/articlesDB')
 const ObjectId = require('mongodb').ObjectID;
-const WeegieDB= require('./../dbClients/weegieQuestionDB')
-const weegieUserAnswer= require('./../dbClients/weegieUserAnswers');
-
+const WeegieDB = require('./../dbClients/weegieQuestionDB')
+ 
 /* GET Articles page. */
 
 router.get('/articles', function (req, res, next) {
@@ -23,38 +22,52 @@ router.get('/articles', function (req, res, next) {
 
 
 });
-router.get('/weegie/user/answer',(req,res)=>{
-    callback=(error,data)=>{
-        if (error) {
-            console.error(error)
-            res.sendStatus(500);
-        }else{
-            res.json(data);            
+
+router.post('/weegie/user/answer', (req, res) => {
+
+    findQuestionCb = (err, data) => {
+
+        if (err) { return err }
+        let similarQuestions = [];
+        let allQuestions = data;
+        let userAnswer = req.body;
+
+        allQuestions.map((question) => {
+            userAnswer.map((answer) => {
+                if (question._id == answer.title) {
+                    similarQuestions.push(question);
+                }
+            })
+        })
+
+        checkAnswers = (questions, answers) => {
+            let result = {};
+            let wrongAnswersList = [];
+            let corretAnswers = 0;
+            let wrongAnswers = 0;
+            answers.map((answer) => {
+                questions.map((question) => {
+                    if (question._id == answer.title) {
+                        if (question.answer === answer.answer) {
+                            corretAnswers++
+                        } else {
+                            wrongAnswers++;
+                            wrongAnswersList.push(question);
+                        }
+                    }
+                })
+            })
+            result.wrongAnswersList = wrongAnswersList;
+            result.correctAnswers = corretAnswers
+            result.wrongAnswers = wrongAnswers;
+            console.log(result)
+            res.send(result)
         }
+        checkAnswers(similarQuestions, userAnswer)
     }
-    weegieUserAnswer.getAnswers({},callback);
+
+    WeegieDB.findQuestions({}, findQuestionCb);
 })
-  router.post('/weegie/user/answer',(req,res)=>{
-    
-    let query = req.body;
-    
-   mapQuery=()=>{
-    var msg="old";
-    
-    query.map((x)=>{
-        
-                  successfullCallback=(err,question)=>{
-                      msg="new"; 
-                  };
-                  WeegieDB.findSingleQuestion(x.title,successfullCallback);   
-              })
-              console.log(msg);
-   }
-   
-   mapQuery();
-        
- //  weegieUserAnswer.createAnswers(query);
- })
 /* GET Single Articles */
 router.get('/articles/:articleId', function (req, res, next) {
     const articleId = req.params.articleId;
@@ -86,37 +99,37 @@ router.get('/categories/:categoryId', function (req, res, next) {
         }
     };
     const { language = "en" } = req.query;
-    
+
     articleClient.findArticles({ category: categoryId, 'visible': true, language: language }, callBack)
 });
 
-router.get('/weegie',(req,res)=>{
+router.get('/weegie', (req, res) => {
 
-    callback=(error,question)=>{
+    callback = (error, question) => {
 
-         shuffle=(array)=> {
+        shuffle = (array) => {
             var currentIndex = array.length, temporaryValue, randomIndex;
-          
+
             // While there remain elements to shuffle...
             while (0 !== currentIndex) {
-          
-              // Pick a remaining element...
-              randomIndex = Math.floor(Math.random() * currentIndex);
-              currentIndex -= 1;
-          
-              // And swap it with the current element.
-              temporaryValue = array[currentIndex];
-              array[currentIndex] = array[randomIndex];
-              array[randomIndex] = temporaryValue;
-            }
-          
-            return  res.json(question);
-          }
 
-          shuffle(question)
-       
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            return res.json(question);
+        }
+
+        shuffle(question)
+
     }
 
-    WeegieDB.findQuestions({},callback);
+    WeegieDB.findQuestions({}, callback);
 })
 module.exports = router;
