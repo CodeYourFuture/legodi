bcb70230192ead5c2e0ff7598e45960cea08a671
DwinaTech <diwina1993@hotmail.com>
2018-01-11 12:42:47 +0000

    Update about page (#174)
    * edited format
    
    * renamed the app
    
    * showed image in article if it has an image under category list
    
    * updated about page and change the app icon

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -4,9 +4,17 @@ import './about.css'
 function About() {
 
     return (
-        <div>
-          <p className="about-text">This application was developed by CodeYourFuture students
-               - Glasgow Cohort 1 - in collaboration with Refuweegee.</p>
+        <div className="container">
+                <p className="about-text">
+                   This application was developed by CodeYourFuture students - 
+                   Glasgow Cohort 1 - in collaboration with Refuweegee.
+               </p>
+                <p className="about-text">
+                            CodeYourFuture is an organisation that teaches programming to refugees and asylum seekers in the UK.
+               </p>
+                <p className="about-text">
+                            We named the app <strong>Legodi</strong> after our friend Simon Basegi <strong>Legodi</strong>, who has been detained by the Home Office for more than 4 months now.
+               </p>     
 
         <div className="about-page">
             <div className="refuweege-logo">
